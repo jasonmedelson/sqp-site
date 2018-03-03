92a61120afdaaef659d7b4223a9bb7d6929feefc
jedelson <edelson.m.jason@gmail.com>
2018-03-03 10:32:50 -0800

    nav bar clear fix & smooth scrolling added

--- js/main.js
+++ js/main.js
@@ -1,50 +1,66 @@
-// $(function() {
-//     // Get the form.
-//     var form = $('#contact-form');
-//
-//     // Get the messages div.
-//     var formMessages = $('#form-messages');
-//
-//     // TODO: The rest of the code will go here...
-//     $(form).submit(function(event) {
-//     // Stop the browser from submitting the form.
-//     event.preventDefault();
-//     });
-//
-//     var formData = $(form).serialize();
-//
-//     $.ajax({
-//     type: 'POST',
-//     url: $(form).attr('action'),
-//     data: formData
-//     })
-//
-//     .done(function(response) {
-//     // Make sure that the formMessages div has the 'success' class.
-//     $(formMessages).removeClass('error');
-//     $(formMessages).addClass('success');
-//
-//     // Set the message text.
-//     $(formMessages).text(response);
-//
-//     // Clear the form.
-//     $('#name').val('');
-//     $('#email').val('');
-//     $('#subject').val('');
-//     $('#message').val('');
-//     })
-//
-//     .fail(function(data) {
-//     // Make sure that the formMessages div has the 'error' class.
-//     $(formMessages).removeClass('success');
-//     $(formMessages).addClass('error');
-//
-//     // Set the message text.
-//     if (data.responseText !== '') {
-//         $(formMessages).text(data.responseText);
-//     } else {
-//         $(formMessages).text('Oops! An error occured and your message could not be sent.');
-//     }
-//     });
-//
-// });
+$(document).ready(function() {
+	enable_smooth_scroll();
+	var w = parseInt(window.innerWidth);
+	if(w >= 990){
+		$("ul.nav").css("display","block");
+	}
+	else{
+		$("ul.nav").css("display","none");
+	}
+	$("a.mini-nav").click(function(){
+		$("ul.nav").slideToggle("slow");
+		return false;
+	});
+	$(window).resize(function(e) {
+        screen_resize();
+	});
+});
+
+function screen_resize() {
+    var w = parseInt(window.innerWidth);
+    if(w >= 990) {
+        $("ul.nav").css("display","block");
+	}
+	else{
+		$("ul.nav").css("display","none");
+	}
+}
+function enable_smooth_scroll(){
+	// Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.not('[href="#myCarousel"]')
+	.click(function(event) {
+	// On-page links
+	if (
+	  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+	  &&
+	  location.hostname == this.hostname)
+	{
+	  // Figure out element to scroll to
+	  var target = $(this.hash);
+	  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	  // Does a scroll target exist?
+	  if (target.length) {
+	    // Only prevent default if animation is actually gonna happen
+	    event.preventDefault();
+	    $('html, body').animate({
+	      scrollTop: target.offset().top - 40
+	    }, 1000, function() {
+	      // Callback after animation
+	      // Must change focus!
+	      var $target = $(target);
+	      $target.focus();
+	      if ($target.is(":focus")) { // Checking if the target was focused
+	        return false;
+	      } else {
+	        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	        $target.focus(); // Set focus again
+	      };
+	    });
+	  }
+	}
+	});
+};
