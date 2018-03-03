5548bcaeabeee2161fd7301b54c5015a62dbe31b
jedelson <edelson.m.jason@gmail.com>
2018-03-02 23:44:35 -0800

    email change

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "contact@jasonmedelson.com";
+$recipient = "edelson.m.jason@gmail.com";
 //$subject = "Contact Form";
 $mailheader = "From: $name \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
